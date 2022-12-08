eb700091fa500abbda8496cab44525868d574e14
Artem Peshkov <pewkoff@gmail.com>
2022-12-08 16:16:12 +0300

    add Ilya code

--- RecipesProject/Services/DataManager.swift
+++ RecipesProject/Services/DataManager.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-struct MyRecipes{
+struct Recipe{
     
     let image: UIImage
     let title: String
@@ -19,50 +19,50 @@ class DataManager {
     
     static let shared = DataManager()
 
-    let recipeArry: [MyRecipes] = [MyRecipes(image:UIImage(named: "1")!,
+    let recipeArry: [Recipe] = [Recipe(image:UIImage(named: "1")!,
                                                  title:"РОЖДЕСТВЕНСКИЙ ШТОЛЛЕН",
                                                  description: "В Германии без штоллена не Рождество! Узнай, почему немцы его любят!"),
-                                   MyRecipes(image:UIImage(named: "2")!,
+                                   Recipe(image:UIImage(named: "2")!,
                                                  title: "КЛАССИЧЕСКИЕ СЫРНИКИ ИЗ ТВОРОГА",
                                                  description: "Классический рецепт пышных, нежных сырников! Пышные сырники приготовить не так то и просто! "),
                                    
-                                   MyRecipes(image:UIImage(named: "3")!,
+                                   Recipe(image:UIImage(named: "3")!,
                                                  title: "ПЫШНЫЕ СЫРНИКИ ИЗ ТВОРОГА С МУКОЙ",
                                                  description: "Пышные и воздушные домашние сырники на завтрак! Сырники - это прекрасный вариант вкусного и сытного завтрака или полдника. "),
                                    
-                                   MyRecipes(image:UIImage(named: "prostaya-sharlotka")!,
+                                   Recipe(image:UIImage(named: "prostaya-sharlotka")!,
                                                  title: "ПРОСТАЯ ШАРЛОТКА С ЯБЛОКАМИ",
                                                  description: "Вкуснейшая, простая, самая лучшая выпечка для всей семьи! Пышная шарлотка с яблоками понравится вам с первого раза."),
                                    
-                                   MyRecipes(image:UIImage(named: "pyshnaya-tvorojnaya-zapekanka")!,
+                                   Recipe(image:UIImage(named: "pyshnaya-tvorojnaya-zapekanka")!,
                                                  title: "ПЫШНАЯ ТВОРОЖНАЯ ЗАПЕКАНКА",
                                                  description: "Легкая, воздушная, тающая во рту запеканка! В этом рецепте я раскрою секреты самой вкусной творожной запеканки с манкой."),
                                    
-                                   MyRecipes(image:UIImage(named: "bliny-na-moloke-tonkie-s-dyrochkami")!,
+                                   Recipe(image:UIImage(named: "bliny-na-moloke-tonkie-s-dyrochkami")!,
                                                  title: "БЛИНЫ НА МОЛОКЕ ТОНКИЕ С ДЫРОЧКАМИ",
                                                  description: "сосиски, яйца, мука пшеничная, мука кукурузная, разрыхлитель, молоко, соль, сахар, масло растительное"),
                                    
-                                   MyRecipes(image:UIImage(named: "bliny-obychnye")!,
+                                   Recipe(image:UIImage(named: "bliny-obychnye")!,
                                                  title: "БЛИНЫ НА КЕФИРЕ",
                                                  description: "Бесподобно вкусно, очень просто и дешево, на каждый день!"),
                                    
-                                   MyRecipes(image:UIImage(named: "blinov-s-dyrochkami-s-kipyatkom")!,
+                                   Recipe(image:UIImage(named: "blinov-s-dyrochkami-s-kipyatkom")!,
                                                  title: "БЛИНЫ С ДЫРОЧКАМИ",
                                                  description: "Ажурная выпечка на кипятке сразу привлечет к себе внимание!"),
                                    
-                                   MyRecipes(image:UIImage(named: "kartofelnje-draniki")!,
+                                   Recipe(image:UIImage(named: "kartofelnje-draniki")!,
                                                  title: "ДРАНИКИ КАРТОФЕЛЬНЫЕ",
                                                  description: " Ну кто же не знает такое популярное блюдо из картофеля, как драники?! "),
                                    
-                                   MyRecipes(image:UIImage(named: "xleb-v-skovorodke")!,
+                                   Recipe(image:UIImage(named: "xleb-v-skovorodke")!,
                                                  title: "ДОМАШНИЙ ХЛЕБ",
                                                  description: "Простой, быстрый, доступный всем способ на каждый день! "),
                                    
-                                   MyRecipes(image:UIImage(named: "pankeiki-na-moloke")!,
+                                   Recipe(image:UIImage(named: "pankeiki-na-moloke")!,
                                                  title: "ПАНКЕЙКИ НА МОЛОКЕ",
                                                  description: "Пробуй лучший рецепт панкейков - идеальный завтрак! "),
                                    
-                                   MyRecipes(image:UIImage(named: "pyshnye-oladi-na-drojjax-i-moloke")!,
+                                   Recipe(image:UIImage(named: "pyshnye-oladi-na-drojjax-i-moloke")!,
                                                  title: "ПЫШНЫЕ ОЛАДЬИ НА ДРОЖЖАХ",
                                                  description: "Вкусные и аппетитные! Просто и очень вкусно!")
                                    
--- RecipesProject/ViewCotrollers/RecipDetailsViewController.swift
+++ RecipesProject/ViewCotrollers/RecipDetailsViewController.swift
@@ -9,16 +9,22 @@ import UIKit
 
 class RecipDetailsViewController: UIViewController {
     
-    var recip: MyRecipes!
+    @IBOutlet var recipeCoverImageView: UIImageView!
+    @IBOutlet var recipeTitleLabel: UILabel!
+    @IBOutlet var recipeDescriptionLabel: UILabel!
+    
+    var recip: Recipe!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = recip.title
+        recipeCoverImageView.image = recip.image
+        recipeTitleLabel.text = recip.title
+        recipeDescriptionLabel.text = recip.description
         view.backgroundColor = .systemBrown
-
-        
     }
-    
+
+
+
 
 
 }
